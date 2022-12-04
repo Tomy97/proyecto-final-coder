cf7bf9a3d99b0ae993b728880be360605fd8d36f
MatiC26 <maticarignano3@gmail.com>
2022-12-04 19:44:28 -0300

    Se agrega models
    :)

--- Campus/models.py
+++ Campus/models.py
@@ -1,2 +1,32 @@
 from django.db import models
+from django.db.models import CharField, DateField, DateTimeField, TextField, IntegerField, EmailField
 
+class UniversidadModels(models.Model):
+    nombre = CharField(max_length=50)
+    localizacion = CharField(max_length=100)
+    zip_code = IntegerField()
+    description = TextField(max_length=2000)
+    contacto = EmailField(max_length=100)
+    
+class AlumnoModels(models.Model):
+    nombre = CharField(max_length=50)
+    apellido = CharField(max_length=50)
+    email = TextField(max_length=100)
+    localidad = CharField(max_length=100)
+    fecha_nac = DateField()
+    contacto = IntegerField()
+
+class ProfesorModels(models.Model):
+    nombre = CharField(max_length=50)
+    apellido = CharField(max_length=50)
+    email = TextField(max_length=100)
+    materia_a_cargo = CharField(max_length=50)
+    
+class UsuarioModels(models.Model):
+    email = CharField(max_length=100)
+    password = CharField(min_length=5, max_length=50)
+    
+class MateriaModels(models.Model):
+    nombre = CharField(max_length=50)
+    nota = CharField(max_length=5)
+    promedio = CharField(max_length=5)
